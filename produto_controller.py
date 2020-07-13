2b2d6abe305be0b8a8e933a71b36f3c02586ecc6
Renan Lordello de Aguiar <renan.aguiar@la.logicalis.com>
2020-07-13 11:08:10 -0300

    creates a controller to list all products

---
+++ produto_controller.py
@@ -0,0 +1,11 @@
+from flask_restful import Resource
+
+from models import Produto as ProdutoModel
+from schemas import ProdutoSchema
+
+class Produto(Resource):
+
+    def get(self):
+        produto_list = ProdutoModel.query.all()
+        return ProdutoSchema(many = True).dump(produto_list)
+        #return [i.name for i in produto_list[0].__table__.columns]
