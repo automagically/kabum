b462ce0d9e3df6825b4c6bb99d5ab99b9f32b00d
Renan Lordello de Aguiar <renan.aguiar@la.logicalis.com>
2020-07-13 11:09:04 -0300

    creates flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+
+from models import db
+from schemas import ma
+from produto_controller import Produto
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mock.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.app = app
+db.init_app(app)
+
+ma.init_app(app)
+
+api = Api(app)
+api.add_resource(Produto, '/produtos')
